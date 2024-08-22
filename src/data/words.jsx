d3ec3c2ce357b22e630a241188c956fba1457f71
eaejaum <jpedroalmeida179@gmail.com>
2024-08-21 23:10:40 -0300

    Criando o projeto

---
+++ src/data/words.jsx
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Câmbio"],
+    fruta: ["Banana", "Maça", "Pêra", "Mamão", "Laranja"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+    computador: ["Mousepad", "Mouse", "Teclado", "Monitor", "Gabinete"],
+    programação: ["Linguagem", "Framework", "Javascript", "React"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+};
